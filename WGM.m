2f8e0036db9b0dcc8d124c2c6a2e816274d723a2
npizzo <npizzo@ucsd.edu>
2021-06-20 18:43:00 -0700

    updates

--- WGM.m
+++ WGM.m
@@ -1,6 +1,6 @@
 function dydt = WGM(t,y,g,nu,beta,kappa,k,omega,a,d,z)
 etax = - a*k*sin(k * y(1)-omega * t);
-hx = - 0*a*k*exp(-k * z)*sin(k * y(3) - omega * t);
+hx = - a*k*exp(-k * z)*sin(k * y(3) - omega * t);
 dydt(1) = y(2);
 dydt(2) = - g * etax - kappa * (y(1)-y(3)-d) + nu * y(2);
 dydt(3) = y(4); 
